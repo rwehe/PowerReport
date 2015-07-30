8dc95af7880692480e873ec5fb53e9cf00d408b4
Ryan <rwehe@asu.edu>
2015-07-30 14:01:03 -0700

    Added the main script file. First try, catch checks for a file at the fileLocation variable and sets the namesFile variable. The first 'if loop' loops through each item contained in namesFile and sets a new variable named computer<number> starting with 1 and counting up for each new variable.

---
+++ PowerConfigs.ps1
@@ -0,0 +1,31 @@
+﻿$computerName = $env:COMPUTERNAME
+
+$fileLocation = "C:\Temp\remoteNames.txt"
+
+
+try
+{
+    $namesFile = Get-Content $fileLocation -ErrorAction Stop
+}
+catch [System.Management.Automation.ItemNotFoundException]{
+    Write-Host "Could not find a file located at $fileLocation`n" -BackgroundColor Black -ForegroundColor Red
+}
+catch{
+    write-host "`n`nCaught an exception:" -ForegroundColor Red
+    write-host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
+    write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red
+}
+
+if ($namesFile){
+    $counter = 0
+    $namesFile | %{
+        $counter++
+        try{
+            Set-Variable -Name "computer$counter" -Value $_
+        }
+        catch {
+            write-host "`n`nCaught an exception:" -ForegroundColor Red
+            Write-Host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red -BackgroundColor Black
+        }
+    }
+}
