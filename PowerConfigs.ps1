d9177fa62e29fb962369f32432d2161dcbd72073
Ryan <rwehe@asu.edu>
2015-08-04 11:18:05 -0700

    Added a Get-PowerReport function and broke out the import-csv file to pass computer names to the function

--- PowerConfigs.ps1
+++ PowerConfigs.ps1
@@ -1,54 +1,93 @@
-﻿$compList = "C:\temp\computers.csv"
-$reportLocation = "C:\Temp\powerTest.csv"
+﻿$hostsPath = "C:\Temp\computers.csv" # Set this variable to point to the location of the CSV file with the hostnames
+$Global:reportLocation = "C:\Power Report.csv" # Set this variable to point to the location of the ouput of this script
 
-Write-Host "Reading from " $compList
-Write-Host "Reporting to " $reportLocation
 
-Import-Csv $compList |
-    foreach{
-        $system = "" | select `
-        "Computer Name",`
-        "Model",`
-        "Operating System",`
-        "Serial Number",`
-        "Energy Consumption (KWh)",`
-        "Instantaneous Headroom (watts)",`
-        "Peak Amperage Reading",`
-        "Peak Headroom (watts)",`
-        "Peak Power Reading (watts)",`
-        "Peak Power Reading (BTU/hrs)",`
-        "Power Supply Current Draw (amps) 1",`
-        "Power Supply Current Draw (amps) 2"
+function Get-PowerReport{
+    param(
+        [string]$computerName,
+        [string]$reportLocation
+    )
 
-        Write-Host "testing" $_.Computer
-        if (Test-Connection $_.Computer -ErrorAction SilentlyContinue){        
-            $chassis = (Get-WmiObject -ComputerName $_.Computer -Class "DELL_Chassis" -Namespace "ROOT\CIMV2\Dell")
-            $PowerConsumptionData = (Get-WmiObject -ComputerName $_.Computer -Class "DELL_PowerConsumptionData" -Namespace "ROOT\CIMV2\Dell")
-            $PowerConsumptionAmpsSensor = (Get-WmiObject -ComputerName $_.Computer -Class "DELL_PowerConsumptionAmpsSensor" -Namespace "ROOT\CIMV2\Dell")
+    Write-Host "working on: $computerName" -ForegroundColor Green -BackgroundColor Black
 
-            $system."Computer Name" = (Get-WmiObject -ComputerName $_.Computer -Class Win32_ComputerSystem).Name
+    $system = "" | select `
+    "Computer Name",`
+    "Model",`
+    "Operating System",`
+    "Serial Number",`
+    "Energy Consumption (KWh)",`
+    "Instantaneous Headroom (watts)",`
+    "Peak Amperage Reading",`
+    "Peak Headroom (watts)",`
+    "Peak Power Reading (watts)",`
+    "Peak Power Reading (BTU/hrs)",`
+    "Power Supply Current Draw (amps) 1",`
+    "Power Supply Current Draw (amps) 2" 
 
-            $system.Model = $chassis.Model
-            $OS = ((Get-WmiObject -ComputerName $_.Computer Win32_OperatingSystem).Name)
-            # The OS variable set above includes the full install path which isn't needed in this case
-            $system."Operating System" = $OS.Substring(0,($OS.IndexOf("|"))) # Grabs the content before the | in the string
+    if (Test-Connection $computerName -ErrorAction SilentlyContinue){ # Pings the host to verify reply
+        try{
+            $chassis = (Get-WmiObject -ComputerName $computerName -Class "DELL_Chassis" -Namespace "ROOT\CIMV2\Dell" -ErrorAction Stop -ErrorVariable e)
+            $PowerConsumptionData = (Get-WmiObject -ComputerName $computerName -Class "DELL_PowerConsumptionData" -Namespace "ROOT\CIMV2\Dell" -EA Stop)
+            $PowerConsumptionAmpsSensor = (Get-WmiObject -ComputerName $computerName -Class "DELL_PowerConsumptionAmpsSensor" -Namespace "ROOT\CIMV2\Dell" -EA Stop)
+
+        }
+        catch [System.Management.ManagementException]{
+            Write-Host "System management exception`nInvalid namespace suspected"
+            $system."Computer Name" = "Attempted: $computerName"
+            $system.Model = "Invalid namespace suspected"
+        }
+        finally{
+            Write-Host "fin"
+        }
+
+#        $chassis = (Get-WmiObject -ComputerName $computerName -Class "DELL_Chassis" -Namespace "ROOT\CIMV2\Dell")
+#        $PowerConsumptionData = (Get-WmiObject -ComputerName $computerName -Class "DELL_PowerConsumptionData" -Namespace "ROOT\CIMV2\Dell")
+#        $PowerConsumptionAmpsSensor = (Get-WmiObject -ComputerName $computerName -Class "DELL_PowerConsumptionAmpsSensor" -Namespace "ROOT\CIMV2\Dell")
+
+        $system."Computer Name" = (Get-WmiObject -ComputerName $computerName -Class Win32_ComputerSystem).Name
+
+        $system.Model = $chassis.Model
+        $OS = ((Get-WmiObject -ComputerName $computerName Win32_OperatingSystem).Name)
+        # The OS variable set above includes the full install path which isn't needed in this case
+        $system."Operating System" = $OS.Substring(0,($OS.IndexOf("|"))) # Grabs the content before the | in the string
         
-            $system."Serial Number" = $chassis.SerialNumber
-            $system."Energy Consumption (KWh)" = $PowerConsumptionData.cumulativePowerReading
-            $system."Instantaneous Headroom (watts)" = $PowerConsumptionData.instHeadRoom
-            $system."Peak Amperage Reading" = $PowerConsumptionData.peakAmpReading / 10
-            $system."Peak Headroom (watts)" = $PowerConsumptionData.peakHeadRoom
-            $system."Peak Power Reading (watts)" = $PowerConsumptionData.peakWattReading
-            $system."Peak Power Reading (BTU/hrs)" = $PowerConsumptionData.peakWattReading * 3.412
+        $system."Serial Number" = $chassis.SerialNumber
+        $system."Energy Consumption (KWh)" = $PowerConsumptionData.cumulativePowerReading
+        $system."Instantaneous Headroom (watts)" = $PowerConsumptionData.instHeadRoom
+        $system."Peak Amperage Reading" = $PowerConsumptionData.peakAmpReading / 10
+        $system."Peak Headroom (watts)" = $PowerConsumptionData.peakHeadRoom
+        $system."Peak Power Reading (watts)" = $PowerConsumptionData.peakWattReading
+        $system."Peak Power Reading (BTU/hrs)" = $PowerConsumptionData.peakWattReading * 3.412
 
-            $system."Power Supply Current Draw (amps) 1" = ($PowerConsumptionAmpsSensor.CurrentReading  | Select -First 1) / 10
-            $system."Power Supply Current Draw (amps) 2" = ($PowerConsumptionAmpsSensor.CurrentReading  | Select -Last 1) / 10
-        }
+        $system."Power Supply Current Draw (amps) 1" = ($PowerConsumptionAmpsSensor.CurrentReading  | Select -First 1) / 10
+        $system."Power Supply Current Draw (amps) 2" = ($PowerConsumptionAmpsSensor.CurrentReading  | Select -Last 1) / 10
+    }
 
-        else {
-            $system."Computer Name" = "Attempted: " + $_.Computer
-            $system."Model" = "Lookup failed"
-        }
+    else{
+        $system."Computer Name" = "Attempted: $computerName"
+        $system."Model" = "Echo request packet failed"
+        Write-Host "Ping to $computerName failed" -ForegroundColor Red
+    }
 
-        $system
-    } | Export-Csv $reportLocation
+    $system | Export-Csv $Global:reportLocation -Append
+    }
+
+# Attempts to read a CSV file located at $hostsPath
+try{
+    $hostsList = Import-Csv $hostsPath -Header Computer -EA SilentlyContinue -ErrorVariable e
+}
+catch [System.IO.FileNotFoundException]{
+    Write-Host "File located at: $hostsPath was not found`n" -BackgroundColor Black -ForegroundColor Red
+    Write-Host "Error was`n$e"
+}
+
+# If-Else to loop through the csv file and run each entry through the Get-PowerReport function
+if($hostsList){
+    Write-Host "Reporting to $Global:reportLocation" -BackgroundColor Black -ForegroundColor DarkGreen
+    Import-Csv $hostsPath | ForEach-Object{
+        Get-PowerReport -computerName ($_.Computer)
+    }
+}
+else{
+    Write-Host "All failed" -ForegroundColor Red -BackgroundColor Black
+}
